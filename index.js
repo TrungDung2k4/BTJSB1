f5f5bf8107142f7378195291d3b5df89e8953353
TrungDung2k4 <140511670+TrungDung2k4@users.noreply.github.com>
2023-08-01 09:10:38 +0700

    Submit Homework

---
+++ index.js
@@ -0,0 +1,53 @@
+// B1
+var luongMoiNgay = 100000
+var soNgay
+var tongLuong
+soNgay = 28
+tongLuong = luongMoiNgay * soNgay
+console.log(tongLuong)
+
+// B2
+var soThuNhat
+var soThuHai
+var soThuBa
+var soThuTu
+var soThuNam
+var giaTriTrungBinh
+soThuNhat = 5.5
+soThuHai = 6.6
+soThuBa = 7.7
+soThuTu = 8.8
+soThuNam = 9.9
+giaTriTrungBinh = (soThuNhat + soThuHai + soThuBa + soThuTu + soThuNam) / 5
+console.log(giaTriTrungBinh)
+
+// B3
+var USD
+var VND
+USD = 1000
+VND = USD * 23500
+console.log(VND)
+
+// B4
+var chieuDai
+var chieuRong
+var chuVi
+var dienTich
+chieuDai = 8
+chieuRong = 7.5
+chuVi = (chieuDai + chieuRong) * 2
+dienTich = chieuDai * chieuRong
+console.log(chuVi)
+console.log(dienTich)
+
+// B5
+var so
+var chuSoHangDonVi
+var chuSoHangChuc
+var tongKySo
+so = 79
+chuSoHangDonVi = so % 10
+so = Math.floor(so / 10)
+chuSoHangChuc = so % 10
+tongKySo = chuSoHangDonVi + chuSoHangChuc
+console.log(tongKySo)
